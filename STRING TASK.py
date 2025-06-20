6c38033bf73bcd84ade58675fd5bbcb8b257f39a
Priyanka Priyadarshini <168274843+priyankapaniAI@users.noreply.github.com>
2025-06-20 23:42:15 +0530

    Add files via upload

---
+++ STRING TASK.py
@@ -0,0 +1,74 @@
+s= 'p'
+print(s)
+print(len(s))
+
+greeting = 'hello world'
+print(type(greeting))
+
+multiline_string = '''I am desperately looking for job.
+I want to see myself as senior AI engineer in the next five years.
+I love to do art and craft and paintings.'''
+print(multiline_string)
+
+multiline_string1 = """ hello how are you all.
+hope you are fine.
+welcome to Naresh IT."""
+print(multiline_string1)
+
+
+language = 'python'
+language= skill= lan1= 'python'
+print(id(language),id(skill),id(lan1))
+
+aaa,bbb,ccc,ddd,eee = 'python','c++','java','nodejs','maven'
+print(aaa)
+print(bbb)
+print(ccc)
+print(ddd)
+
+s = 'pinki'
+s1,s2,s3,s4,s5= s
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)
+
+s= 'python'
+print(s[0:])
+print(s[0:3])
+print(s[3:6])
+print(s[-1])
+print(s[0:6:2])
+print(s[2:8:2])
+
+print('INDIA\tfaced\ta\ttragic\tloss\tdue\tto\tAIR\tINDIA\tplane\tcrash')
+
+print("Name\tAge\tCity")
+print("Alice\t25\tNew York")
+print("Bob\t30\tLondon")
+
+print(s[::-1]) #reversing
+
+string= 'datascience with AI'
+print(string.capitalize())
+print(string.count('a'))
+print(string.find('e')) # it will find the index of 'e'
+print(string.find('da'))
+print(string.count('c',5,10)) #it will count the alphabet c from index 5 to 10
+print(string.endswith('ai'))
+print(string.endswith('AI'))
+print(string.upper())
+print(string.lower())
+print(string[0:12]+string[12:16].upper()+string[16:19])
+
+print(string.swapcase())
+print(string.title())
+print(string.split())
+print(string.replace('with','and'))
+print(string.strip('I')) #removes beginning and end of the string not in the middle
+s= ['war', 'begins', 'between', 'Iran', 'and', 'Israel']
+print('  '.join(s))
+
+
+
