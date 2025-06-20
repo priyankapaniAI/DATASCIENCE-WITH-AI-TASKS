9767f767117e85b729f402ed89da819f5eb19a2c
Priyanka Priyadarshini <168274843+priyankapaniAI@users.noreply.github.com>
2025-06-20 16:15:35 +0530

    Add files via upload

---
+++ operatorTASK.py
@@ -0,0 +1,58 @@
+print('Addition: ', 1 + 2)
+print('Subtraction: ', 2 - 1)
+print('Multiplication: ', 2 * 3)
+print ('Division: ', 4 / 2) 
+print('Division: ', 6 / 2)
+print('Division: ', 7 / 2)
+print('division',7//2)
+print('modulus',3%2)
+print('exponential',3**2)
+
+radius= 10
+area_of_circle= 3.14 * 10**2
+print('area of the circle',area_of_circle)
+
+length= 10
+breadth= 20
+area_of_rectangle= length*breadth
+print('area of rectangle',area_of_rectangle)
+
+print(3 > 2)
+print(3 >= 2)
+print(3 < 2)
+print(2 < 3)
+print(2 <= 3)
+print(3 == 2)
+print(3 != 2)
+
+print(len('mango') == len('avocado'))
+print(len('mango') != len('avocado'))
+print(len('mango') < len('avocado'))
+print(len('milk') != len('meat')) 
+print(len('milk') == len('meat'))
+print(len('tomato') == len('potato'))
+print(len('python') > len('dragon')) 
+
+
+print(3 > 2 and 4 > 3)
+print(3 > 2 and 4 < 3)
+print(3 < 2 and 4 < 3)
+print(3 > 2 or 4 > 3)
+print(3 > 2 or 4 < 3)
+print(3 < 2 or 4 < 3)
+print(not 3 > 2)
+print(not True) 
+print(not False)
+print(not not True)
+print(not not False)
+
+
+print('is this true', 1 is 1)
+print('what will be the answer',1 is not 2)           
+print('A' in 'python')
+print('B' in 'naresh')
+print('coding' in 'coding for all')
+print('a' in 'an')
+print( 4 is 2 ** 2)
+
+
