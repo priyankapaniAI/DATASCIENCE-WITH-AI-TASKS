5280fe2d7e12c9ea57a63243b84c48f2d70c262d
Priyanka Priyadarshini <168274843+priyankapaniAI@users.noreply.github.com>
2025-06-20 16:11:31 +0530

    Add files via upload

---
+++ variablesTASK.py
@@ -0,0 +1,16 @@
+first_name = 'Priyanka'
+last_name = 'pani'
+city = 'bengaluru'
+skills = ['aws','devops','data science', 'ai']
+age = 30
+is_married = True
+
+print('my first name is',first_name)
+print('last name is',last_name)
+print('my skills are',skills)
+print('are you married',is_married)
+print('my age :',age)
+
+first_name, last_name, country, age, is_married = 'priyanka','pani','India',30,True
+print(first_name, last_name, country, age, is_married)
+print(f'my name is {first_name} and lived in {city}')
