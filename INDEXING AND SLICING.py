09ce0121e95bef57aefdea9897f38272cb0ac9db
Priyanka Priyadarshini <168274843+priyankapaniAI@users.noreply.github.com>
2025-06-21 17:58:24 +0530

    Add files via upload

---
+++ INDEXING AND SLICING.py
@@ -0,0 +1,10 @@
+s = 'datascience'
+print(s[0])         #d
+print(s[len(s)-1])  #e
+print(s[-1])  #e
+print(s[6])   #i
+print(s[-5])  #i
+print(s[0:5]) #datas
+print(s[6:12]) #ience
+print(s[-4:])  #ence
+print(s[:-6])  #datas
